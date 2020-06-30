0504f20ce457193510d2e708ec99f3c19ff86fb2
maxjerdee <mjerdee@princeton.edu>
2020-06-30 19:44:37 -0400

    AA

--- index.js
+++ index.js
@@ -1,13 +1,12 @@
-const express = require('express');
+const express = require('express')
+const path = require('path')
+const PORT = process.env.PORT || 5000
 
-const router = express.Router();
-
-router.get('/', (req, res) => {
-  res.render('form', { title: 'Registration forms'});
-});
-router.post('/', (req, res) => {
-    console.log(req.body);
-    res.render('form', { title: 'Registration form' });
-  });
-
-module.exports = router;
+express()
+  .use(express.static(path.join(__dirname, 'public')))
+  .set('views', path.join(__dirname, 'views'))
+  .set('view engine', 'pug')
+  .get('/', (req, res) => res.render('form'))
+  .get('/plants', (req, res) => res.render('pages/plants'))
+  .get('/blog-sample', (req, res) => res.render('pages/blog'))
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
