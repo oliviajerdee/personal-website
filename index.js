e97fb4a11d5f42ba82411f0555fc8b7950808ba1
maxjerdee <mjerdee@princeton.edu>
2020-06-30 20:37:35 -0400

    AA

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ express()
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'pug')
+  .get('/', (req, res) => {
+    res.render('pages/form')})
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
 
-router.get('/', (req, res) => {
-  res.render('pages/form')});
