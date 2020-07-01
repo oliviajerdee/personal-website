e28cf977cf891eba4c6405efa948311e12c25ec6
maxjerdee <mjerdee@princeton.edu>
2020-07-01 00:56:45 -0400

    set views

--- index.js
+++ index.js
@@ -5,6 +5,6 @@ const PORT = process.env.PORT || 5000
 express()
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', __dirname + '/views')
-  .set('view engine', 'pub')
+  .set('view engine', 'pug')
   .get('/', (req, res) => res.render('form'))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
