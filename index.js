387c379821b0e38c71b2ca6fb00f91d989252778
maxjerdee <mjerdee@princeton.edu>
2020-07-01 11:35:46 -0400

    Pug Server Testing

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const express = require('express')
 const path = require('path')
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 4000
 
 console.log(__dirname);
 
 express()
-  .use(express.static(path.join(__dirname, 'public')))
+  .use(express.static('./public'))
   .set('views', './views')
-  .get('/', (req, res) => res.render('pages/form'))
-  .get('/blog', (req, res) => res.render('pages/blog'))
+  .set('view engine', 'pug')
+  .get('/', (req, res) => res.render('pages/index'))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
